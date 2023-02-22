9e9a151ac30d59e921291ba90bbd45e06ec1dc31
Chukwu Stanley <stanleychukwu17@yahoo.com>
2023-02-22 08:51:53 +0100

    done with the home page design

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,26 @@
 import './app.scss';
-import { motion } from 'framer-motion';
-import { gsap } from 'gsap';
+// import { motion } from 'framer-motion';
+// import { gsap } from 'gsap';
+
+
+// from react-icons
+import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
 
 const App = () => {
-
     return (
         <div className="AppMain">
-
+            <div className="header">
+                <div className="HLink">Products</div>
+                <div className="HMenu">
+                    <div className=""><AiOutlineMenu/></div>
+                    <div className=""><AiOutlineClose/></div>
+                </div>
+            </div>
+            <div className="">
+                <div className="HomePageDts">
+                    Click on the menu button
+                </div>
+            </div>
         </div>
     )
 }
